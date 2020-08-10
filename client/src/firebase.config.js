a8689a0a42faebdf351c3f500a94898060c89a9b
BH Partners <dev@bhpartners.io>
2020-08-10 09:51:33 +0000

    Updated firebase.config.js to Nisa Credentials

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: 'AIzaSyDyk5Jo0mBtaaL_YF7xaD-mYhOUykbHezg',
-  authDomain: 'project-ocr-282210.firebaseapp.com',
-  databaseURL: 'https://project-ocr-282210.firebaseio.com',
-  projectId: 'project-ocr-282210',
-  storageBucket: 'project-ocr-282210.appspot.com',
-  messagingSenderId: '471665923189',
-  appId: '1:471665923189:web:c4e07c49e90245953c143c',
-  measurementId: 'G-L98ZGPPMTF',
+  apiKey: "AIzaSyCjV3WckkaAWBxbN6DoaKtms-OQUz7Jchk",
+  authDomain: "project-ocr-150620.firebaseapp.com",
+  databaseURL: "https://project-ocr-150620.firebaseio.com",
+  projectId: "project-ocr-150620",
+  storageBucket: "project-ocr-150620.appspot.com",
+  messagingSenderId: "985519715978",
+  appId: "1:985519715978:web:d2d3c28c96d0cd59fadf73",
+  measurementId: "G-XQ1DTWJJGF",
 }
