b52f87edcde45cae6d5376e0828ae784e91c8c52
zombieteer <aryan.nigam1996@gmail.com>
2020-12-19 20:49:41 +0530

    mail sent added

--- NDscript.js
+++ NDscript.js
@@ -14,6 +14,7 @@ const btnClickHandler = async (event, sdk) => {
   let sender, loggedInUser, attachment;
   if (event.position === "THREAD") {
     loggedInUser = await sdk.User.getEmailAddress();
+
     await sdk.Conversations.registerMessageViewHandler(async (messageView) => {
       sender = messageView.getSender();
       attachment =
@@ -26,6 +27,24 @@ const btnClickHandler = async (event, sdk) => {
               ._attachmentCardImplementation._element
           : [];
     });
+
     console.log(sender, loggedInUser, attachment);
+
+    Email.send({
+      SecureToken: "ce8a1081-19cd-4ab5-bc49-8b0f9b893d64",
+      // Host: "smtp.gmail.com",
+      // Username: "nisadoclock@gmail.com",
+      // Password: "nisafinance",
+      To: loggedInUser,
+      From: "nisadoclock@gmail.com",
+      Subject: "This is the subject",
+      Body: "And this is the body",
+    }).then((message) => {
+      if (message === "OK") {
+        alert("Protected file has been reverted to your mail");
+      } else {
+        alert("Mail sending failed");
+      }
+    });
   }
 };
