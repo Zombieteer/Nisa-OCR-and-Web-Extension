c7bddca44f12554792c7efbdfda1d20c8970feba
zombieteer <aryan.nigam1996@gmail.com>
2020-12-22 23:21:24 +0530

    modals added

--- NDscript.js
+++ NDscript.js
@@ -1,4 +1,7 @@
-InboxSDK.load(2, "sdk_NesaDocLock_70621e1bb4").then(function (sdk) {
+let inboxSdkId = "sdk_NesaDocLock_70621e1bb4";
+let mailClientSecureToken = "ce8a1081-19cd-4ab5-bc49-8b0f9b893d64";
+
+InboxSDK.load(2, inboxSdkId).then(function (sdk) {
   // the SDK has been loaded, now do something with it!
   sdk.Toolbars.registerThreadButton({
     title: "Encrypt File",
@@ -10,102 +13,126 @@ InboxSDK.load(2, "sdk_NesaDocLock_70621e1bb4").then(function (sdk) {
   });
 });
 
-const btnClickHandler = async (event, sdk) => {
-  let sender, loggedInUser;
+const getAttachments = async (sdk) => {
   let attachments = [];
+  let sender;
+  await sdk.Conversations.registerMessageViewHandler(async (messageView) => {
+    sender = messageView.getSender();
+
+    messageView.getFileAttachmentCardViews().length &&
+      messageView.getFileAttachmentCardViews().forEach((item) => {
+        let attachment = item._attachmentCardImplementation._element;
+        let download_url = attachment.attributes.getNamedItem("download_url")
+          .textContent;
+        let re = /([^:]+):([^:]+):(.+)/;
+        let match = re.exec(download_url);
+
+        const contentType = match[1];
+        const name = decodeURI(match[2]);
+        const contentLink = match[3];
+
+        fetch(contentLink)
+          .then((response) => {
+            const reader = response.body.getReader();
+            return new ReadableStream({
+              start(controller) {
+                return pump();
+                function pump() {
+                  return reader.read().then(({ done, value }) => {
+                    if (done) {
+                      controller.close();
+                      return;
+                    }
+                    controller.enqueue(value);
+                    return pump();
+                  });
+                }
+              },
+            });
+          })
+          .then((stream) => new Response(stream))
+          .then((response) => response.blob())
+          .then((blob) => {
+            let file = new File([blob], name, { type: contentType });
+            // const url = URL.createObjectURL(file);
+
+            // let link = document.createElement("a");
+            // link.href = url;
+            // link.setAttribute("download", "file.pdf");
+            // link.click();
+          })
+          .catch((err) => console.error(err));
+
+        attachments.push({
+          name: `Nisa_Encrypted_${name}`,
+          path: contentLink,
+        });
+
+        console.log(match);
+      });
+  });
+  return {attachments, sender};
+};
+
+const sendMail = (sdk, loggedInUser, attachments) => {
+  const showModal = (title, message) => {
+    let mail_sent_modal = sdk.Widgets.showModalView({
+      title,
+      el: `<div>${message}</div>`,
+      buttons: [
+        {
+          text: "Ok",
+          type: "PRIMARY_ACTION",
+          showCloseButton: true,
+          onClick: () => mail_sent_modal.close(),
+        },
+      ],
+    });
+    document.querySelector(".inboxsdk__modal_content").style.margin = 0;
+  };
+
+  Email.send({
+    SecureToken: mailClientSecureToken,
+    // Host: "smtp.gmail.com",
+    // Username: "nisadoclock@gmail.com",
+    // Password: "nisafinance",
+    To: loggedInUser,
+    From: "nisadoclock@gmail.com",
+    Subject: "Protected file reverted by NisaDoclock",
+    Body:
+      "Download the attachment from this mail and upload it as your documents in NisaFinance",
+    Attachments: attachments,
+  }).then((message) => {
+    if (message === "OK") {
+      showModal("Success", "Mail has been reverted, please check inbox");
+    } else {
+      showModal(
+        "Failed / Error",
+        "Mail sending failed, please try again or contact support@nisafinance.com"
+      );
+    }
+  });
+};
+
+const btnClickHandler = async (event, sdk) => {
+  let loggedInUser;
   if (event.position === "THREAD") {
     loggedInUser = await sdk.User.getEmailAddress();
 
-    await sdk.Conversations.registerMessageViewHandler(async (messageView) => {
-      sender = messageView.getSender();
-
-      messageView.getFileAttachmentCardViews().length &&
-        messageView.getFileAttachmentCardViews().forEach((item) => {
-          let attachment = item._attachmentCardImplementation._element;
-          let download_url = attachment.attributes.getNamedItem("download_url")
-            .textContent;
-          let re = /([^:]+):([^:]+):(.+)/;
-          let match = re.exec(download_url);
-
-          let contentType = match[1];
-          let name = decodeURI(match[2]);
-          let contentLink = match[3];
-
-          fetch(contentLink)
-            .then((response) => {
-              const reader = response.body.getReader();
-              return new ReadableStream({
-                start(controller) {
-                  return pump();
-                  function pump() {
-                    return reader.read().then(({ done, value }) => {
-                      if (done) {
-                        controller.close();
-                        return;
-                      }
-                      controller.enqueue(value);
-                      return pump();
-                    });
-                  }
-                },
-              });
-            })
-            .then((stream) => new Response(stream))
-            .then((response) => response.blob())
-            .then((blob) => {
-              let file = new File([blob], name, { type: contentType });
-              // const url = URL.createObjectURL(file);
-              
-              // let link = document.createElement("a");
-              // link.href = url;
-              // link.setAttribute("download", "file.pdf");
-              // link.click();
-            })
-            .catch((err) => console.error(err));
-
-          attachments.push({
-            name: `Nisa_Encrypted_${name}`,
-            path: contentLink,
-          });
-
-          console.log(match);
-        });
+    let attachment_read_modal = sdk.Widgets.showModalView({
+      title: "Please Wait...",
+      el: "<div>Attachments are being read</div>",
     });
+    document.querySelector(".inboxsdk__modal_content").style.marginTop = 0;
 
-    Email.send({
-      SecureToken: "ce8a1081-19cd-4ab5-bc49-8b0f9b893d64",
-      // Host: "smtp.gmail.com",
-      // Username: "nisadoclock@gmail.com",
-      // Password: "nisafinance",
-      To: loggedInUser,
-      From: "nisadoclock@gmail.com",
-      Subject: "Protected file reverted by NisaDoclock",
-      Body:
-        "Download the attachment from this mail and upload it as your documents in NisaFinance",
-      Attachments: attachments,
-    }).then((message) => {
-      if (message === "OK") {
-        alert("Protected file has been reverted to your mail");
-      } else {
-        alert(
-          "Mail sending failed, please try again or contact support@nisafinance.com"
-        );
-      }
-    });
+    // get attachments
+    let {attachments, sender} = await getAttachments(sdk);
+
+    attachment_read_modal.close();
+
+    // encrypt file
+
+    // send mail
+    sendMail(sdk, loggedInUser, attachments);
   }
 };
-
-// var el =
-// '<div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"' +
-// '<div class="modal-header">' +
-// '<h3 id="myModalLabel">Attach Files</h3></div>' +
-// '<div class="modal-body">' +
-// '<form id="fileUploadForm" method="post" enctype="multipart/form-data">' +
-// '<input type="file" name="FileAttachement[]" multiple="true" id="FileInput" class="form-control" value="FileInput" />' +
-// "</form>" +
-// '<input type="submit" class="fileAttachementSubmitButton">' +
-// "</div></div>";
-
-// sdk.Widgets.showModalView({
-// el,
-// });
